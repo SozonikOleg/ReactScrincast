1598ab34737f4212f875556c87c6997e6c8f8781
SozonikOleg <sozonik999@gmail.com>
2019-02-08 13:40:05 +0300

    docs:add data for article

---
+++ src/fixtures.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        "id": "1",
+        "date": "2016-06-09t15:03:23",
+        "title": "This is Title",
+        "text": "Commodo qui incididunt ex ut ea nulla et eu aliquip duis Commodo qui incididunt ex ut ea nulla et eu aliquip duis Commodo qui incididunt ex ut ea nulla et eu aliquip duis Commodo qui incididunt ex ut ea nulla et eu aliquip duis Commodo qui incididunt ex ut ea nulla et eu aliquip duis Commodo qui incididunt ex ut ea nulla et eu aliquip duis Commodo qui incididunt ex ut ea nulla et eu aliquip duis Commodo qui incididunt ex ut ea nulla et eu aliquip duis Commodo qui incididunt ex ut ea nulla et eu aliquip duis Commodo qui incididunt ex ut ea nulla et eu aliquip duis Commodo qui incididunt ex ut ea nulla et eu aliquip duis Commodo qui incididunt ex ut ea nulla et eu aliquip duis Commodo qui incididunt ex ut ea nulla et eu aliquip duis Commodo qui incididunt ex ut ea nulla et eu aliquip duis ",
+        "comments": [
+            {
+                "id": "1 111111",
+                "user": "111111Gilliam Underwood",
+                "text": "111Velit anim deserunt elit velit est Velit anim deserunt elit velit est Velit anim deserunt elit velit est"
+            },
+            {
+                "id": "1 22222",
+                "user": "1 222Gilliam Underwood",
+                "text": "1 222Velit anim deserunt elit velit est Velit anim deserunt elit velit est Velit anim deserunt elit velit est"
+            },
+            {
+                "id": "1 33333",
+                "user": "1 33333 Gilliam Underwood",
+                "text": "1 3333Velit anim deserunt elit velit est Velit anim deserunt elit velit est Velit anim deserunt elit velit est"
+            },
+            {
+                "id": "1 444444",
+                "user": "1 44444Gilliam Underwood",
+                "text": "1 4444Velit anim deserunt elit velit est Velit anim deserunt elit velit est Velit anim deserunt elit velit est"
+            },
+        ]
+    }
+]
